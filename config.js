3ff3acdabcdf7cde61b34b5bfbac39c427fc9505
turbotugatuga-lgtm <turbotugatuga@gmail.com>
2025-09-03 23:46:47 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 🔑 API keys configuration
 window.CONFIG = {
-  SHYFT_API_KEY: "VzPp9y_hw4dFfmfF",
-  BIRDEYE_API_KEY: "22febf5b37e34452af2cc632dff69b88"
+  SHYFT_API_KEY: "VzPp9y_hw4dFfmfF",                 // Shyft API key
+  BIRDEYE_API_KEY: "22febf5b37e34452af2cc632dff69b88" // Birdeye API key
 };
