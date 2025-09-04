3d45a9b3b9885025793f5e2110ddd985dc81c796
turbotugatuga-lgtm <turbotugatuga@gmail.com>
2025-09-04 09:50:12 -0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// 🔑 Configuração de chaves
-window.CONFIG = {
-  SHYFT_API_KEY: "VzPp9y_hw4dFfmfF",   // <-- sua chave Shyft
-  BIRDEYE_API_KEY: "22febf5b37e34452af2cc632dff69b88" // <-- sua chave Birdeye
-};
